7011ceb6a7786a87e840b8b3eda865312f1a5aa4
Micael Persson <micael.c.persson@gmail.com>
2014-10-13 14:43:10 +0200

    Added properties in Shape.cs

--- 1DV402.S2.L03A/1DV402.S2.L03A/Shape.cs
+++ 1DV402.S2.L03A/1DV402.S2.L03A/Shape.cs
@@ -12,5 +12,40 @@ namespace _1DV402.S2.L03A
         private double _length;
         private double _height;
 
+        // Properties
+        public abstract double Area
+        {
+            get;
+        }
+
+        public double Length
+        {
+            get { return _length; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Talet får inte vara negativt");
+                }
+                else { _length = value; }
+            }
+        }
+
+        public abstract double Perimeter
+        {
+            get;
+        }
+
+        public abstract double Height
+        {
+            get { return _height; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Talet får inte vara negativt");
+                }
+            }
+        }
     }
 }
