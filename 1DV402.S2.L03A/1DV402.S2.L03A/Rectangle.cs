e50e55d639a4374a797890ec95254a982ceb262e
Micael Persson <micael.c.persson@gmail.com>
2014-10-15 11:29:18 +0200

    Area property for Rectangle.cs

---
+++ 1DV402.S2.L03A/1DV402.S2.L03A/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L03A
+{
+    class Rectangle : Shape
+    {
+        public override double Area
+        {
+            get { return Length * Height; }
+        }
+    }
+}
