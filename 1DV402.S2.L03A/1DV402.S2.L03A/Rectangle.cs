f5d2f0a94b0e02ab7ac2e89eddc5b1f2ba009045
Micael Persson <micael.c.persson@gmail.com>
2014-10-15 11:31:41 +0200

    Perimeter property for Rectangle.cs

--- 1DV402.S2.L03A/1DV402.S2.L03A/Rectangle.cs
+++ 1DV402.S2.L03A/1DV402.S2.L03A/Rectangle.cs
@@ -8,9 +8,15 @@ namespace _1DV402.S2.L03A
 {
     class Rectangle : Shape
     {
+        // Returning rectangle area
         public override double Area
         {
             get { return Length * Height; }
+        }
+        // Returning rectangle perimeter
+        public override double Perimeter
+        {
+            get { return (2 * Height) + (2 * Length); }
         }
     }
 }
