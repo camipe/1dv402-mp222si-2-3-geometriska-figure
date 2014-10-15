f684a27cc5f338a49314ac1d241f73cf119bf9a2
Micael Persson <micael.c.persson@gmail.com>
2014-10-15 11:08:06 +0200

    Perimeter property in Ellipse.cs

--- 1DV402.S2.L03A/1DV402.S2.L03A/Ellipse.cs
+++ 1DV402.S2.L03A/1DV402.S2.L03A/Ellipse.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace _1DV402.S2.L03A
 {
-    class Ellipse
+    class Ellipse : Shape
     {
+        // Returning area
+        public override double Area
+        {
+            get { return Length * Height * Math.PI; }
+        }
+
+        // Returning perimeter
+        public override double Perimeter
+        {
+            get { return Math.PI * Math.Sqrt(2 * (Length * 2) * 2 * (Height * 2));  }
+        }
     }
 }
