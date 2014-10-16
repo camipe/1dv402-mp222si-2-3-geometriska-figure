5a13aa1ffe449f8b5d13253df7c4e1567fbbf12e
Micael Persson <micael.c.persson@gmail.com>
2014-10-16 12:29:29 +0200

    Fixed a problem with the ellipse calculations.

--- 1DV402.S2.L03A/1DV402.S2.L03A/Ellipse.cs
+++ 1DV402.S2.L03A/1DV402.S2.L03A/Ellipse.cs
@@ -11,13 +11,13 @@ namespace _1DV402.S2.L03A
         // Returning area
         public override double Area
         {
-            get { return Length * Width * Math.PI; }
+            get { return Length / 2 * Width / 2 * Math.PI; }
         }
 
         // Returning perimeter
         public override double Perimeter
         {
-            get { return Math.PI * Math.Sqrt(2 * (Length * 2) * 2 * (Width * 2));  }
+            get { return Math.PI * Math.Sqrt((2 * (Length / 2) * (Length / 2)) + (2 * (Width / 2) * (Width / 2))); }
         }
         // Constructor
         public Ellipse(double length, double width)  
